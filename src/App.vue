e96894bb73437b037f3fcb9e163f6f221404d7c4
dongjiancheng <dongjiancheng@meituan.com>
2023-05-27 14:43:59 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br/>
+    <a-button type="primary">
+      Primary
+    </a-button>
+
+    <br/>
+    MTD
+    <mtd-button type="primary">基础按钮</mtd-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue'
+import MTD from '@ss/mtd-vue'
 import App from './App.vue'
+import '@ss/mtd-vue/lib/theme2/index.css'
+import 'ant-design-vue/dist/antd.css';
+
+Vue.use(MTD)
+Vue.use(Antd)
 
 Vue.config.productionTip = false
 
